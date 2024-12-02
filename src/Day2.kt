c600070bd623f38bafcef5cff53bf00b7f3c050e
mvauderw <wz.wahffle@gmail.com>
2024-12-02 08:52:36 +0100

    Finished day 2

---
+++ src/Day2.kt
@@ -0,0 +1,57 @@
+class Day2 {
+    private enum class Direction {
+        ASC,
+        DESC,
+        NONE
+    }
+
+    private fun preProc(input: List<String>): List<List<Int>> =
+        input.map { it.split(" ").map { it2 -> it2.toInt() } }
+
+    private fun isSafe(input: List<Int>, direction: Direction): Boolean {
+        return when (direction) {
+            Direction.ASC -> input.foldIndexed(true) { idx, acc, next ->
+                idx == 0 || (acc && next > input[idx - 1] && next <= input[idx - 1] + 3)
+            }
+
+            Direction.DESC -> input.foldIndexed(true) { idx, acc, next ->
+                idx == 0 || (acc && next < input[idx - 1] && next >= input[idx - 1] - 3)
+            }
+
+            Direction.NONE -> false
+        }
+    }
+
+    private fun generateReducedLists(input: List<Int>): List<List<Int>> {
+        return input.foldIndexed(listOf<List<Int>>()) { idx, acc, _ ->
+            listOf(*acc.toTypedArray(), input.filterIndexed { index, _ -> idx != index })
+        }
+    }
+
+    private fun findDirection(input: List<Int>): Direction {
+        for (i in input) {
+            if (input.first() < i) {
+                return Direction.ASC
+            }
+            if (input.first() > i) {
+                return Direction.DESC
+            }
+        }
+
+        return Direction.NONE
+    }
+
+    fun part1(input: List<String>) {
+        println(preProc(input).map {
+            isSafe(it, findDirection(it))
+        }.count { it })
+    }
+
+    fun part2(input: List<String>) {
+        println(preProc(input).map {
+            generateReducedLists(it).any { it2 ->
+                isSafe(it2, findDirection(it2))
+            }
+        }.count { it })
+    }
+}
