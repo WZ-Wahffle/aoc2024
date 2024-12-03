5a916185707603f8f3047a90c62e9a26f59de7e6
mvauderw <wz.wahffle@gmail.com>
2024-12-03 08:26:23 +0100

    Finished day 3

---
+++ src/Day3.kt
@@ -0,0 +1,33 @@
+class Day3 {
+    fun part1(input: List<String>) {
+        println("""mul\((?<first>\d{1,3}),(?<second>\d{1,3})\)""".toRegex()
+            .findAll(input.joinToString(""))
+            .fold(0) { acc, next ->
+                acc + (next.groups["first"]!!.value.toInt() * next.groups["second"]!!.value.toInt())
+            }
+        )
+    }
+
+    fun part2(input: List<String>) {
+        var doAdd = true
+        println("""mul\((?<first>\d{1,3}),(?<second>\d{1,3})\)|do\(\)|don't\(\)""".toRegex()
+            .findAll(input.joinToString(""))
+            .fold(0) { acc, next ->
+                when (next.value) {
+                    "do()" -> {
+                        doAdd = true
+                        acc
+                    }
+
+                    "don't()" -> {
+                        doAdd = false
+                        acc
+                    }
+
+                    else -> {
+                        acc + if (doAdd) (next.groups["first"]!!.value.toInt() * next.groups["second"]!!.value.toInt()) else 0
+                    }
+                }
+            })
+    }
+}
